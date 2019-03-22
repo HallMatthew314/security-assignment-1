8b47245de84ae08dd8a0b00ee325dc6bb2516202
Matthew Hall <hallmg1@student.op.ac.nz>
2019-03-22 14:28:35 +1300

    Change extraction format to be TeX friendly

--- resources/shadow-info-extractor.rb
+++ resources/shadow-info-extractor.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
 SHADOW = 'death-star-shadow.txt'
-OUTFILE = 'extracted-' + SHADOW + '.csv'
+OUTFILE = 'extracted-' + SHADOW
 INFO_PATTERN = /^([a-z_0-9-]+):\$\d\$([A-Za-z0-9.\/]+)\$([A-Za-z0-9.\/]+)/
 
 entries = File.read(SHADOW)
@@ -16,6 +16,6 @@ entries.each do |e|
   fields.push(INFO_PATTERN.match(e))
 end
 
-CSV.open(OUTFILE, 'w') do |csv|
-  fields.each { |f| csv << [f[1], f[2], f[3]] }
+File.open(OUTFILE, 'w') do |file|
+  fields.each { |f| file << f[1] + ' & p & ' + f[3] + ' & ' + f[2] + " \\\\\n\\hline\n" }
 end
